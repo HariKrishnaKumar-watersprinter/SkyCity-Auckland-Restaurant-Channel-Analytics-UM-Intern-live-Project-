e3e41076001a3e379c11a8e0bc60f6e66359e3fe
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-03-18 21:32:37 +0530

    Update insights_dashboard.py

--- dashboards/insights_dashboard.py
+++ dashboards/insights_dashboard.py
@@ -9,3 +9,11 @@ def show(df):
 
     for ins in insights:
         st.write("•", ins)
+    if st.button("Generate Executive Report"):
+        path = generate_report(df)
+
+        with open(path, "rb") as f:
+            st.download_button(
+                "Download Report",
+                    f,
+                file_name="SkyCity_Report.pdf")
