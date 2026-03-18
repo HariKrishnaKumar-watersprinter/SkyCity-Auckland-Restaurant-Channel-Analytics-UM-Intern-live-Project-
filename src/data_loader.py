20f7c4b8d84f4c13d150e018101fe586d53d1629
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-03-18 14:37:00 +0530

    Reformat data_loader.py for consistency

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 @st.cache_data
-def load_data(path=r"https://raw.githubusercontent.com/HariKrishnaKumar-watersprinter/SkyCity-Auckland-Restaurant-Channel-Analytics-UM-Intern-live-Project-/main/data/SkyCity Auckland Restaurants & Bars.csv"):
+def load_data(path="https://raw.githubusercontent.com/HariKrishnaKumar-watersprinter/SkyCity-Auckland-Restaurant-Channel-Analytics-UM-Intern-live-Project-/main/data/SkyCity Auckland Restaurants & Bars.csv"):
     df = pd.read_csv(path)
     return df
 
