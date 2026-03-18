9bee77c87deac935dd91135e1b187a9926a08bd0
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-03-18 14:41:31 +0530

    Refactor load_data function to use a fixed path

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
-
+path="https://raw.githubusercontent.com/HariKrishnaKumar-watersprinter/SkyCity-Auckland-Restaurant-Channel-Analytics-UM-Intern-live-Project-/main/data/SkyCity Auckland Restaurants & Bars.csv"
 @st.cache_data
-def load_data(path="https://raw.githubusercontent.com/HariKrishnaKumar-watersprinter/SkyCity-Auckland-Restaurant-Channel-Analytics-UM-Intern-live-Project-/main/data/SkyCity Auckland Restaurants & Bars.csv"):
+def load_data():
     df = pd.read_csv(path)
     return df
 
