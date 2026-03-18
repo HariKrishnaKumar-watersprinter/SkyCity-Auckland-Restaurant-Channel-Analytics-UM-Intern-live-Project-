594caf007199f6a73c7ffb8c806e6ec6cc10bdaa
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-03-18 14:45:41 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-path="https://raw.githubusercontent.com/HariKrishnaKumar-watersprinter/SkyCity-Auckland-Restaurant-Channel-Analytics-UM-Intern-live-Project-/main/data/SkyCity Auckland Restaurants & Bars.csv"
+path="https://raw.github.com/HariKrishnaKumar-watersprinter/SkyCity-Auckland-Restaurant-Channel-Analytics-UM-Intern-live-Project-/blob/main/data/SkyCity%20Auckland%20Restaurants%20%26%20Bars.csv"
 @st.cache_data
 def load_data():
     df = pd.read_csv(path)
